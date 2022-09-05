b7363931109bb2b20b7b85aa5a76417086ad9039
YehosuaEs <yehosuaes@gmail.com>
2022-09-05 12:59:20 +0200

    prework class and objects

---
+++ prework/ClassAndObjects/ClassObjects.java
@@ -0,0 +1,15 @@
+package ClassAndObjects;
+
+public class ClassObjects {
+    public static void main(String[] args) {
+        Book book1 = new Book("Harry Potter", "JK Rowling", 54);
+        Book book2 = new Book("Moby Dick", "Herman Melville", 378);
+        
+        System.out.println("The book " + book1.getTitle() + " is " + book1.getLength() + " pages long.");
+        
+        book2.BookDescription();
+
+
+    }
+    
+}
