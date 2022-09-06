ba9dca18a886d112d5adc0b919e691bfc4a33baa
YehosuaEs <yehosuaes@gmail.com>
2022-09-06 16:08:37 +0200

    inheritance with the word super

---
+++ prework/Super/Person.java
@@ -0,0 +1,32 @@
+package Super;
+
+class Person {
+    private String name;
+    private int age;
+
+    //contructor
+    public Person(String name, int age){
+        setName(name);
+        setAge(age);
+    };
+    // setters
+    public void setName(String name) {
+        this.name = name;     
+    };
+    public void setAge(int age) {
+        this.age = age;
+    };
+    // getters
+    public String getName(){
+        return this.name;
+    };
+    public int getAge(){
+        return this.age;
+    };
+
+    // methods
+    public void sayHi() {
+        System.out.println("Hello I'm "+ getName());
+    }
+
+};
---
+++ prework/Super/Super.java
@@ -0,0 +1,14 @@
+package Super;
+
+class Super {
+    public static void main(String[] args) {
+        Person person1 = new Person("Fran", 25);
+        Employee employee1 = new Employee("Yehosua", 30, "UI developer");
+
+        System.out.println(person1.getName());
+        System.out.println(employee1.getName());
+        person1.sayHi();
+        employee1.sayHi();
+    }
+    
+}
